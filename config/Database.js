0bdd12b5b45ce0c77cd134899e5ae0b03a5e0b76
IrfanNasyakban <irvan.26102001@gmail.com>
2025-08-02 11:54:27 +0700

    push config

--- config/Database.js
+++ config/Database.js
@@ -2,6 +2,7 @@ const { Sequelize } = require("sequelize");
 
 const db = new Sequelize('db_lms', 'admin_greensys', 'greensys', {
     host: '31.97.49.141',
+    port: 3306,
     dialect: 'mysql'
 })
 
