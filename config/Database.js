76bf002676ef5f6cc935608036daf215aeb27142
Irvan Na'syakban <70449673+IrfanNasyakban@users.noreply.github.com>
2025-09-09 11:25:45 +0700

    Update Database.js

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require("sequelize");
 
-const db = new Sequelize('db_lms', 'root', '', {
-    host: 'localhost',
+const db = new Sequelize('db_lms', 'admin_greensys', 'greensys', {
+    host: '31.97.49.141',
     port: 3306,
     dialect: 'mysql'
 })
